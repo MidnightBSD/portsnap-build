124fa48d407568d4e83e1f4bfdd1afc0b33aad85
Lucas Holt <luke@foolishgames.com>
2021-08-18 13:50:58 -0400

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-umount /home/portsnap/portsnap-build/state/work/tmp/jail/dev /home/portsnap/portsnap-build/state/work/tmp/jail/usr/ports  /home/portsnap/portsnap-build/state/work/tmp/jail/tmp /home/portsnap/portsnap-build/state/work/tmp/jail /home/portsnap/portsnap-build/state/work/snap /home/portsnap/portsnap-build/state/work/tmp/mports
+umount /home/portsnap/portsnap-build/state/work/tmp/jail/dev /home/portsnap/portsnap-build/state/work/tmp/jail/usr/mports  /home/portsnap/portsnap-build/state/work/tmp/jail/tmp /home/portsnap/portsnap-build/state/work/tmp/jail /home/portsnap/portsnap-build/state/work/snap /home/portsnap/portsnap-build/state/work/tmp/mports
 rm -r state/work
 rm failed
